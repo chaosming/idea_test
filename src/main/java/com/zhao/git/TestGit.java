37f52919fbb62cc01f6f1aea8a3fd5537f4270ed
zhao <10174@qq.com>
2023-08-24 11:08:19 +0800

    第二次更新

--- src/main/java/com/zhao/git/TestGit.java
+++ src/main/java/com/zhao/git/TestGit.java
@@ -4,5 +4,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
+
     }
 }
